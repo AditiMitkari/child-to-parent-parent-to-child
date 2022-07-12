ca6a88f80e13629d700596fc2f1d362bef64002c
AditiMitkari <mitkariaditi3@gmail.com>
2022-07-12 15:19:21 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'child-parent-parent-child';
+
+public Data : any;
+
+public Message = "Hello Child.."
+
+}
